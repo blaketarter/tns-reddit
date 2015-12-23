4fd687e3802970ad56e432347c4ad9e7419ce113
Blake Tarter <blake@codekoalas.com>
2015-12-23 14:54:22 -0600

    changed code so that the layout is built in the code instead of the xml to allow for more flexablilty

--- app/views/front/front.js
+++ app/views/front/front.js
@@ -4,10 +4,20 @@ var http = require('fetch');
 var dialog = require("ui/dialogs");
 var observable = require("data/observable");
 var observableArray = require("data/observable-array");
+var moment = require('moment');
+
 var view = require("ui/core/view");
 var actionBarModule = require("ui/action-bar");
-var moment = require('moment');
+var listViewModule = require("ui/list-view");
+var labelModule = require("ui/label");
+var stackLayoutModule = require("ui/layouts/stack-layout");
+var imageModule = require("ui/image");
+
+var enums = require("ui/enums");
+var orientation = enums.Orientation;
+
 var page;
+var posts = [];
 
 var pageData = new observable.Observable({
     posts: new observableArray.ObservableArray([
@@ -24,28 +34,91 @@ exports.loaded = function(args) {
       })
       .then(function(data) {
         data.data.children.map(function(post) {
-          post.data.created = moment(post.data.created_utc * 1000).fromNow();
-          pageData.posts.push(post);
+          post.data.created = formatDate(post.data.created_utc);
+          posts.push(post.data);
         });
 
-        setTimeout(() => {
+        console.log(posts);
 
-return;
-          var postElems = view.getViewById(page, 'posts');
-          console.log(postElems);
-        
-          for (let post of postElems._map.values()) {
-            console.log(post);
-            if (!post._subViews[0]._subViews[0]._url || !post._subViews[0]._subViews[0]._url.length) {
-              console.log('no url');
-              post._subViews[0].removeChild(post._subViews[0]._subViews[0]);
-              
-              setTimeout(() => {
-                post._subViews[0]._subViews[0].left = 12;
-              }, 0);
-            }
-          }
-        }, 0);
+        pageData.posts = new observableArray.ObservableArray(posts);
+
+        buildListView(view.getViewById(page, 'list-layout'), pageData.posts);
       });
 };
 
+function formatDate(rawDate) {
+  return moment(rawDate * 1000).fromNow();
+}
+
+function buildListView(pageView, posts) {
+  var listView = new listViewModule.ListView();
+  listView.items = posts;
+  listView.on(listViewModule.ListView.itemLoadingEvent, function(args) {
+    if (!args.view) {
+      let post = posts.getItem(args.index);
+      args.view = buildListItem(post);
+    }
+  });
+
+  pageView.addChild(listView);
+  pageView.refresh();
+}
+
+function buildStackLayout(dir, children) {
+  var stackLayout = new stackLayoutModule.StackLayout();
+  stackLayout.orientation = orientation[dir];
+
+  if (children) {
+    for (let child of children) {
+      stackLayout.addChild(child);
+    }
+  }
+
+  return stackLayout;
+}
+
+function buildListItem(post) {
+  return buildStackLayout('vertical', [
+    buildTitlePart(post),
+    buildStackLayout('horizontal', [
+      buildLabel(post.author, 'author'),
+      buildLabel(post.created, 'created'),
+      buildLabel(post.subreddit, 'subreddit')
+    ])
+  ]);
+}
+
+function buildCachedImage(src, className) {
+  let image = new imageModule.Image();
+  image.src = src;
+  if (className) {
+    image.className = className;
+  }
+
+  return image;
+}
+
+function buildTitlePart(post) {
+  var titlePart = [];
+
+  if (post.thumbnail && post.thumbnail.length) {
+    titlePart.push(buildCachedImage(post.thumbnail, 'image'));
+  } else {
+    console.log('no image');
+  }
+
+  titlePart.push(buildLabel(post.title, 'title'));
+
+  return buildStackLayout('horizontal', titlePart);
+}
+
+function buildLabel(text, className) {
+  let label = new labelModule.Label();
+  label.text = text;
+
+  if (className) {
+    label.className = className;
+  }
+
+  return label;
+}
